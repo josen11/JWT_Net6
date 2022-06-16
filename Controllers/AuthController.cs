679b312770007b3a94f29ca39dbd01936327dd8e
José Enrique Aguirre Chavez <jotae_11@hotmail.com>
2022-06-16 15:06:24 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace JWT_Net6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        // Para testear esta parte primero register y luego como ese valor se asociara a la instancia usuario
+        // nos logueamos con la contraseña que salio, ya que el evento register ya asocia el password generado 
+        // a la instancia de usuario que creamos
+        public async Task<ActionResult<User>> Register(UserDTO request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.UserName = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDTO request)
+        {
+            if (user.UserName != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            //Interesante utilizamos un array de claims que nos ayudara a enviar la data hacia el JWT
+            //Un claim va a sser como que un dato de usuario 
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.UserName),
+            };
+            //OJO el SymmetricSecurityKey se basa en Microsoft.IdentityModel.Tokens;
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+               _configuration.GetSection("AppSettings:Token").Value));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            //OJO el JwtSecurityToken se basa en System.IdentityModel.Tokens.Jwt;;
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred
+                );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+
+        //Create Password Hash and salt method
+        //Ambos nos ayudara a formar el JWT 
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computeHash=hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computeHash.SequenceEqual(passwordHash); // Genial para comparar y devolver un bool
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,10 @@
+﻿namespace JWT_Net6
+{
+    public class User
+    {
+        public string UserName { get; set; } =  string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}
